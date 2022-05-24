cf31b5fc244b2b4c9571e4a9ab2af08eecf66978
RSMarian <marianstefan1499@gmail.com>
2022-05-24 12:54:59 +0300

    added start button functionality

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ let seconds = 00;
 let tens = 00;
 let getSeconds = document.querySelector('.seconds');
 let getTens = document.querySelector('.tens');
-let btnStart = document.querySelectonr('.btn-start');
-let btnStop = document.querySelectonr('.btn-stop');
-let btnReset = document.querySelectonr('.btn-reset');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+
+btnStart.addEventListener('click',()=>{
+    setInterval(startTimer, 10)
+})
+
+function startTimer(){
+    tens++;
+    if(tens <= 9){
+        getTens.innerHTML = '0' + tens;
+    }
+    if(tens > 9){
+        getTens.innerHTML = tens;
+    }
+    if(tens > 99){
+        seconds++;
+        getSeconds.innerHTML = '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+    if(seconds > 9){
+        getSeconds.innerHTML = seconds;
+    }
+}
