2b34f78bf90da5c92824685d98bfd54fd1cb4078
RSMarian <marianstefan1499@gmail.com>
2022-05-24 12:12:03 +0300

    forgot to save

---
+++ app.js
@@ -0,0 +1,7 @@
+let seconds = 00;
+let tens = 00;
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let btnStart = document.querySelectonr('.btn-start');
+let btnStop = document.querySelectonr('.btn-stop');
+let btnReset = document.querySelectonr('.btn-reset');
